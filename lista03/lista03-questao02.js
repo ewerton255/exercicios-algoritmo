5f802a1416a8b70d670e79653c588147dc8f1a38
ewerton255 <e.d.correia@hotmail.com>
2018-09-04 18:56:36 -0300

    resolução questão 03

---
+++ lista03/lista03-questao02.js
@@ -0,0 +1,20 @@
+function questao02() {
+
+    let dia = parseInt(window.prompt(`
+    1- Domingo
+    2- Segunda
+    3- Terça
+    4- Quarta
+    5- Quinta
+    6- Sexta
+    7- Sábado   
+    Insira o número do dia da semana:`))
+
+    if (dia > 1 && dia < 7) {
+        console.log('É um dia útil!')
+    } else if (dia == 1 || dia == 7) {
+        console.log('É final de semana!')
+    } else {
+        console.log('Dia inválido!')
+    }
+}

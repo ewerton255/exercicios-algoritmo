4a1b235246d3869dda6259c2d6c28e881e6dd217
ewerton255 <e.d.correia@hotmail.com>
2018-09-04 18:47:01 -0300

    resolução questão 01 da lista 03

--- lista03/lista03-questao01.js
+++ lista03/lista03-questao01.js
@@ -1,3 +1,9 @@
-function questao01(){
-    
+function questao01() {
+    let numero = parseInt(window.prompt('Insira um número inteiro: '))
+
+    if (numero % 3 == 0) {
+        console.log('É divisível!')
+    } else {
+        console.log('Não é divisível!')
+    }
 }
